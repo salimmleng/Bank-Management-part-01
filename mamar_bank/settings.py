fcf9c39a79d1b10f01c81c1a546e35d4d64d85af
Salim Miah <salimmiah0785@gmail.com>
2024-06-15 13:38:59 -0700

    updated

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -141,6 +141,8 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
